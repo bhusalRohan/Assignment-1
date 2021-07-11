ce4f434af39bfaabd393afa506d0e37ff386ff3b
Bhusal Rohan <rohanbhusal@gmail.com>
2021-07-11 21:28:11 +1000

    Final version

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { Colours } from './components/Colours'
+import { Header } from './components/Header'
+import { List } from './components/List'
+
+const appDefintion = {
+  name: "My ToDo List",
+}
+
+const storageKey = 'listdata'
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    if (!data) {
+      // read from storage
+      // setData( data from storage )
+    }
+    else {
+      // save into storage
+    }
+  }, [data])
+
+  const addItem = (itemName) => {
+    let itemId = new Date().getTime()
+    let Item = { name: itemName, id: itemId, status: false }
+    setData(data.concat(Item))
+  }
+
+  const markItemDone = (itemId) => {
+    const items = [...data] // [ {item1} , {item2} ]
+    items.forEach((item) => {
+      if (item.id === itemId) {
+        item.status = true
+      }
+    })
+    // set the items array as the data
+    setData(items)
+  }
+
+  const deleteItem = (itemId) => {
+    //console.log( itemId )
+    // copy the data state
+    const items = [...data]
+    items.forEach((item, index) => {
+      if (item.id === itemId) {
+        items.splice(index, 1)
+      }
+    })
+    setData(items)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Rohan ToDo App; my first native project</Text>
-      <StatusBar style="auto" />
+      <Header name={appDefintion.name} addHandler={addItem} />
+      <List listItems={data} doneHandler={markItemDone} deleteHandler={deleteItem} />
     </View>
   );
 }
@@ -14,8 +68,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: Colours.light,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
